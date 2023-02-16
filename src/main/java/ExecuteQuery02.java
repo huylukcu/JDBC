a54c17ee2e7298081108632a8f7bf890c7328268
hayriye uylukcu <huylukcu@gmail.comgit config --global user.email huylukcu@gmail.comgit config --global user.name hayriye>
2023-02-16 15:27:10 -0600

    commit

--- src/main/java/ExecuteQuery02.java
+++ src/main/java/ExecuteQuery02.java
@@ -32,13 +32,8 @@ public class ExecuteQuery02 {
         while (resultSet2.next()) {
 
             System.out.println(resultSet2.getString("country_id") + "->" + resultSet2.getString("country_name"));
-
         }
         //3.Example: Select the company whose number_of_employees is the lowest from companies table
         String sql3="SELECT company FROM companies WHERE number_of_employees = (SELECT min(number_of_employees)FROM companies)";
-    }
-}
-
-
     }
 }
